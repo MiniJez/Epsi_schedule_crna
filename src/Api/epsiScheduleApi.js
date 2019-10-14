768a38332708bb3422a697cc471b3483666e1d79
Edouard CLISSON <edouardclisson@gmail.com>
2019-10-14 16:45:42 +0200

    Fix: change api call name

--- src/Api/epsiScheduleApi.js
+++ src/Api/epsiScheduleApi.js
@@ -1,6 +1,6 @@
 
 export async function getEpsiScheduleDay(date) {
-    const url = `http://eclisson.duckdns.org:3000/schedule/date?date=${date}`
+    const url = `http://eclisson.duckdns.org:3000/schedule/day?date=${date}`
     //return [{"id":"0","date":"Lundi 14 Octobre","matiere":"LANGAGE DE PROGRAMMA","debut":"08:00","fin":"12:00","salle":"CG 213","prof":"MULOT MATHIEU"},{"id":"1","date":"Lundi 14 Octobre","matiere":"LANGAGE DE PROGRAMMA","debut":"13:00","fin":"17:00","salle":"CG 213","prof":"MULOT MATHIEU"}]
     return fetch(url, {method: 'GET'}).then(
         (response) => response.json().then(
